11a9aeb9173d3f8ff7cf6e06711c0c4e9a102f95
popcorn1752 <james@Jamess-MBP.gateway>
2017-01-14 21:46:25 +1000

    Change

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         } else {
             $.ajax({
                 method: 'POST',
-                url: '//formspree.io/your@email.com',
+                url: '//formspree.io/tester1752@gmail.com',
                 data: $('#contact-form').serialize(),
                 datatype: 'json'
             });
