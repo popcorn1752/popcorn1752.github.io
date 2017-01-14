98f0c5a460dd5fe16cfda0723a72833e4051d789
popcorn1752 <james@Jamess-MBP.gateway>
2017-01-14 21:33:22 +1000

    Change

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $("#data").submit(function(e) {
+    $("#data").submit(function() {
         alert("submitted");
         $.ajax({
             url: "https://formspree.io/tester1752@gmail.com",
