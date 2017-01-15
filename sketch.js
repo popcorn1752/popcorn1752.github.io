057c180b389216dee26b81e2ed46e53977196355
popcorn1752 <james@Jamess-MBP.gateway>
2017-01-15 19:15:07 +1000

    JSON File Drop down list

--- sketch.js
+++ sketch.js
@@ -2,30 +2,37 @@ var data;
 
 function load() {
     $.getJSON("https://popcorn1752.github.io/data.json", function(x) {
-        console.log(x);
         data = x;
+
+        for (var i = 0; i < data.stores.kallangur.length; i++) {
+            //console.log(data.stores.kallangur[i].Name);
+            var name = (data.stores.kallangur[i].Name);
+
+            var newOption = "<option value=" + name + ">" + name + "</option>";
+            $("#nameSelect").append(newOption);
+        }
     });
 }
 
 $(document).ready(function() {
     load();
-    $('#contact-form').submit(function() {
-        var name = document.getElementById('inputName')
-        var tet = document.getElementById('inputTET')
-        var message = document.getElementById('inputMessage')
-
-        if (!name.value || !tet.value || !message.value) {
-            alert("Please check your entries");
-            return false;
-        } else {
-            $.ajax({
-                method: 'POST',
-                url: "https://formspree.io/tester1752@gmail.com",
-                data: $('#contact-form').serialize(),
-                datatype: 'json'
-            });
-
-            alert("Message sent");
-        }
-    });
+    // $('#contact-form').submit(function() {
+    //     var name = document.getElementById('inputName')
+    //     var tet = document.getElementById('inputTET')
+    //     var message = document.getElementById('inputMessage')
+    //
+    //     if (!name.value || !tet.value || !message.value) {
+    //         alert("Please check your entries");
+    //         return false;
+    //     } else {
+    //         $.ajax({
+    //             method: 'POST',
+    //             url: "https://formspree.io/tester1752@gmail.com",
+    //             data: $('#contact-form').serialize(),
+    //             datatype: 'json'
+    //         });
+    //
+    //         alert("Message sent");
+    //     }
+    // });
 });
