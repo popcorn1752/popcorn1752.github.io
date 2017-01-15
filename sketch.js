9065a3f18eaa1b8213180dd089ac897c4c4a7adc
popcorn1752 <james@Jamess-MBP.gateway>
2017-01-15 19:16:51 +1000

    Change

--- sketch.js
+++ sketch.js
@@ -16,23 +16,23 @@ function load() {
 
 $(document).ready(function() {
     load();
-    // $('#contact-form').submit(function() {
-    //     var name = document.getElementById('inputName')
-    //     var tet = document.getElementById('inputTET')
-    //     var message = document.getElementById('inputMessage')
-    //
-    //     if (!name.value || !tet.value || !message.value) {
-    //         alert("Please check your entries");
-    //         return false;
-    //     } else {
-    //         $.ajax({
-    //             method: 'POST',
-    //             url: "https://formspree.io/tester1752@gmail.com",
-    //             data: $('#contact-form').serialize(),
-    //             datatype: 'json'
-    //         });
-    //
-    //         alert("Message sent");
-    //     }
-    // });
+    $('#contact-form').submit(function() {
+        var name = document.getElementById('inputName')
+        var tet = document.getElementById('inputTET')
+        var message = document.getElementById('inputMessage')
+
+        if (!name.value || !tet.value || !message.value) {
+            alert("Please check your entries");
+            return false;
+        } else {
+            $.ajax({
+                method: 'POST',
+                url: "https://formspree.io/tester1752@gmail.com",
+                data: $('#contact-form').serialize(),
+                datatype: 'json'
+            });
+
+            alert("Message sent");
+        }
+    });
 });
